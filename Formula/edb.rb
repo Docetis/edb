2bd144b046518cf9e1f84c26a14ccebbbde57aff
Luca Carnevale-Breguet <luca.carnevale-breguet@swisscom.com>
2025-12-01 15:28:34 +0100

    added SHA

--- Formula/edb.rb
+++ Formula/edb.rb
@@ -2,7 +2,7 @@ class Edb < Formula
   desc "eXist-DB CLI toolkit – export/import, watch-sync, XAR build, backup & rollback"
   homepage "https://github.com/Docetis/edb"
   url "https://github.com/Docetis/edb/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "7961e67aae35363572d4a79bf653d194781ad2208153bd7eb012f88e70d84dc9"
   license "MIT"
 
   depends_on "curl"
